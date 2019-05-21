04f002b6a76da6f28228f8505de0b3551a984626
Ethiraric <floriansabourin@live.fr>
2019-05-21 20:22:43 +0200

    Make it an error to not consume the whole symbol.

--- src/details/Parser.cpp
+++ src/details/Parser.cpp
@@ -1,5 +1,7 @@
 #include <demangler/details/Parser.hh>
 
+#include <stdexcept>
+
 #include <demangler/details/node/MangledName.hh>
 
 namespace demangler
@@ -11,7 +13,11 @@ std::unique_ptr<Node> Parser::parse(std::string const& symbol)
   if (!node::MangledName::isMangled(symbol))
     return nullptr;
   State s{symbol, nullptr, {}, false, {}, {}, {}};
-  return node::MangledName::parse(s);
+  auto ret = node::MangledName::parse(s);
+  if (!s.empty())
+    throw std::runtime_error{"Symbol not entirely consumed: " +
+                             gsl::to_string(s.symbol)};
+  return ret;
 }
 }
 }
